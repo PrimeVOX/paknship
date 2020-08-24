b0d4a7de866c6bad40bc01188f9bf5b2168fca6e
Aaron <aaron@primevox.net>
2020-08-24 15:28:07 -0500

    keep stuff separate

--- dist/cli.js
+++ dist/cli.js
@@ -15,7 +15,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs_extra_1 = require("fs-extra");
 const rimraf_1 = __importDefault(require("rimraf"));
 const api_1 = require("./api");
-const titere_1 = require("titere");
 const utils_1 = require("./utils");
 function cli() {
     // handle args
@@ -30,7 +29,6 @@ function cli() {
     Pak'n'Ship
     Commands:
       invoice /path/to/file.json    Send invoice(s) using specified file.
-      inline  <url|string>          Generate & return buffer for inline PDF.
     Options:
       -h, --help    displays help.
     `;
@@ -71,28 +69,6 @@ function cli() {
             rimraf_1.default(path, () => {
                 process.exit();
             });
-        }))();
-    }
-    ///////////////////////////////
-    // INLINE
-    ///////////////////////////////
-    if (cmd === 'inline') {
-        const urlOrHtml = argv.shift();
-        // can only accept one incoming param, all others ignored
-        if (!urlOrHtml) {
-            process.stderr.write('A URL must be specified.', 'utf-8');
-            process.exit();
-        }
-        (() => __awaiter(this, void 0, void 0, function* () {
-            const buf = yield titere_1.inline(urlOrHtml);
-            if (typeof buf === 'boolean' && buf === false) {
-                process.stderr.write('PDF could not be created.\n', 'utf-8');
-                process.exit();
-            }
-            else {
-                process.stdout.write(buf, 'utf-8');
-                process.exit();
-            }
         }))();
     }
 }
--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,6 @@
 import { pathExists, readJson } from 'fs-extra';
 import rimraf from 'rimraf';
 import { invoice } from './api';
-import { inline } from 'titere';
 import { me } from './utils';
 
 function cli() {
@@ -22,7 +21,6 @@ function cli() {
     Pak'n'Ship
     Commands:
       invoice /path/to/file.json    Send invoice(s) using specified file.
-      inline  <url|string>          Generate & return buffer for inline PDF.
     Options:
       -h, --help    displays help.
     `;
@@ -75,37 +73,6 @@ function cli() {
       rimraf(path, () => {
         process.exit();
       });
-
-    })();
-
-  }
-
-  ///////////////////////////////
-  // INLINE
-  ///////////////////////////////
-
-  if (cmd === 'inline') {
-
-    const urlOrHtml = argv.shift();
-
-    // can only accept one incoming param, all others ignored
-    if (!urlOrHtml) {
-      process.stderr.write('A URL must be specified.', 'utf-8');
-      process.exit();  
-    }
-
-    (async () => {
-
-      const buf = await inline(urlOrHtml);
-
-      if (typeof buf === 'boolean' && buf === false) {
-        process.stderr.write('PDF could not be created.\n', 'utf-8');
-        process.exit();
-      }
-      else {
-        process.stdout.write(buf, 'utf-8');
-        process.exit();
-      }
 
     })();
 
